c25aabef81416e500ac336df7718108f75af87eb
Ansh Bindroo <bindrooansh@gmail.com>
2023-02-07 10:06:04 +0400

    learnt how to create pages

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import sqlite3
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Great stuff<p>"
+    return "<p>Harihar Rengan for Head of CS Soc<p>"
+
+@app.route("/movies")
+def ant_man():
+    return render_template('movies.html')
+
+@app.route("/tv")
+def tlou():
+    return render_template('tv.html')
