98a659a7a61ed19b58219c588701e985b9b51970
Ansh Bindroo <bindrooansh@gmail.com>
2023-02-26 21:50:31 +0400

    made a small fix and started commenting

--- website/views.py
+++ website/views.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 
+# The below code allows different routes to be stored.
 views = Blueprint("views", __name__)
 
 @views.route("/")
 @views.route("/home")
 @login_required
 def home():
-    return render_template("home.html", name="Ansh")
+    return render_template("home.html", name=current_user.usernamet)
