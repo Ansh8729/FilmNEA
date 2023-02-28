1735dc9cb53c99c5f559a178503a75b3f59f784c
Ansh Bindroo <bindrooansh@gmail.com>
2023-02-28 09:16:04 +0400

    added even more comments

--- website/views.py
+++ website/views.py
@@ -6,6 +6,8 @@ views = Blueprint("views", __name__)
 
 @views.route("/")
 @views.route("/home")
-@login_required
+@login_required # This line restricts users from going on the home page without logging in.
 def home():
-    return render_template("home.html", name=current_user.usernamet)
+    return render_template("home.html", name=current_user.username) 
+    #This line renders the home page using the HTML code in "home.html"
+    
