f23312755d8584908cb09107a172384aecc2c3bc
Ansh Bindroo <bindrooansh@gmail.com>
2023-06-30 16:03:08 +0400

    finished modelling database, needs fixing though

--- website/__init__.py
+++ website/__init__.py
@@ -22,7 +22,7 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
         
-    from .models import User
+    from .models import Users
     
     with app.app_context():
         db.create_all()
@@ -33,7 +33,7 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(id):
-        return User.query.get(int(id))
+        return Users.query.get(int(id))
 
     return app 
 
--- website/models.py
+++ website/models.py
@@ -8,6 +8,8 @@ class Users(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     data_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    writers = db.relationship('Screenwriters', backref = 'Users')
+    producers = db.relationship('Producers', backref = 'Users')
 
 class Screenwriters(db.Model):
     writerid = db.Column(db.Integer, primary_key=True)
@@ -17,6 +19,10 @@ class Screenwriters(db.Model):
     backgroundcolour = db.Column(db.Integer)
     fontstyle = db.Column(db.Integer)
     experiencelevel = db.Column(db.Float)
+    screenplays = db.relationship('Screenplays', backref = 'Screenwriters')
+    submissions = db.relationship('Submissions', backref = 'Screenwriters')
+    responses = db.relationship('SubmissionResponses', backref = 'Screenwriters')
+    liked = db.relationship('LikedScreenplays', backref = 'Screenwriters')
 
 class BGColours(db.Model):
     backgroundcolour = db.Column(db.Integer, primary_key=True)
@@ -35,21 +41,27 @@ class Screenplays(db.Model):
     screenplay = db.Column(db.String(300)) #TO BE CHANGED
     data_created = db.Column(db.DateTime(timezone=True), default=func.now())
     avgrating = db.Column(db.Float)
+    scripthas = db.relationship('ScriptHas', backref = 'Screenplays')
+    requests = db.relationship('Requests', backref = 'Screenplays')
+    liked = db.relationship('LikedScreenplays', backref = 'Screenplays')
 
 class ScriptHas(db.Model):
-    scriptid = db.Column(db.Integer, primary_key=True)
+    scriptid = db.Column(db.Integer, db.ForeignKey('Screenplays.scriptid'), primary_key=True)
     genreid = db.Column(db.Integer, primary_key=True)
 
-class ScriptHas(db.Model):
+class Genres(db.Model):
     genreid = db.Column(db.Integer, primary_key=True)
     genre = db.Column(db.String(50))
 
 class Producers(db.Model):
     producerid = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.Integer, db.ForeignKey('Users.userid'))
-    profilepic = db.Column(db.String(150))
+    profilepic = db.Column(db.String(150)) # TO BE CHANGED
     biography = db.Column(db.Text)
     approved = db.Column(db.Integer)
+    requests = db.relationship('Requests', backref = 'Producers')
+    competitions = db.relationship('Competitions', backref = 'Producers')
+    responses = db.relationship('SubmissionResponses', backref = 'Producers')
 
 class Requests(db.Model):
     requestid = db.Column(db.Integer, primary_key=True)
@@ -57,5 +69,36 @@ class Requests(db.Model):
     scriptid = db.Column(db.Integer, db.ForeignKey('Screenplays.scriptid'))
     granted = db.Column(db.Integer)
 
+class Competitions(db.Model):
+    compid = db.Column(db.Integer, primary_key=True)
+    producerid = db.Column(db.Integer, db.ForeignKey('Producers.producerid'))
+    title = db.Column(db.String(300))
+    brief = db.Column(db.Text)
+    deadline = db.Column(db.DateTime(timezone=True))
+    comphas = db.relationship('CompHas', backref = 'Competitions')
+    submissions = db.relationship('Submissions', backref = 'Competitions')
+    responses = db.relationship('SubmissionResponses', backref = 'Competitions')
 
+class CompHas(db.Model):
+    compid = db.Column(db.Integer, db.ForeignKey('Competitions.compid'), primary_key=True)
+    genreid = db.Column(db.Integer, primary_key=True)
 
+class Submissions(db.Model):
+    writerid = db.Column(db.Integer, db.ForeignKey('Screenwriters.writerid'), primary_key=True)
+    compid = db.Column(db.Integer, db.ForeignKey('Competitions.compid'), primary_key=True)
+    submission = db.Column(db.String(150)) # TO BE CHANGED
+    submissiondate = db.Column(db.DateTime(timezone=True))
+
+class SubmissionResponses(db.Model):
+    producerid = db.Column(db.Integer, db.ForeignKey('Producers.producerid'), primary_key=True)
+    writerid = db.Column(db.Integer, db.ForeignKey('Screenwriters.writerid'), primary_key=True)
+    compid = db.Column(db.Integer, db.ForeignKey('Competitions.compid'), primary_key=True)
+    message = db.Column(db.Text)
+
+class LikedScreenplays(db.Model):
+    writerid = db.Column(db.Integer, db.ForeignKey('Screenwriters.writerid'), primary_key=True)
+    scriptid = db.Column(db.Integer, db.ForeignKey('Screenplays.scriptid'), primary_key=True)
+    title = db.Column(db.String(300))
+    rating = db.Column(db.Float)
+    commented = db.Column(db.Integer)
+    
