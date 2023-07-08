23b30212b818ae152ab831e57040a608bff694d5
Ansh Bindroo <bindrooansh@gmail.com>
2023-07-08 22:08:46 +0400

    created form and page for profile page editor

--- website/views.py
+++ website/views.py
@@ -1,8 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 from .models import Users, Screenwriters, Producers
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, FileField, TextAreaField, SelectField
 
 views = Blueprint("views", __name__)
+
+class ProfilePageForm(FlaskForm):
+    profilepic = FileField("Profile Picture")
+    biography = TextAreaField("Biography")
+    bgcolour = SelectField("Background Colour", choices=[('1', 'Red'), ('2', 'Yellow'), ('3', 'Blue')])
+    fontstyle = SelectField("Font Style", choices=[('1', 'Default'), ('2', 'Arial'), ('3', 'Impact')])
+    submit = SubmitField("Submit")
 
 @views.route("/")
 @views.route("/home")
@@ -16,17 +25,14 @@ def profilepage():
     user = Users.query.filter_by(username = current_user.username).first()
     if user.accounttype == 1:
         details = Screenwriters.query.filter_by(userid = user.id).first()
-    if user.accounttype == 2:
+    elif user.accounttype == 2:
         details = Producers.query.filter_by(userid = user.id).first()
+
     return render_template("profilepage.html", user=current_user, details1=user, details2=details)
 
-'''
-@views.route("/<username>/profilepage")
+@views.route("/pageeditor", methods=['GET', 'POST'])
 @login_required
-def profilepage():
-    if current_user.accounttype == 1:
-        writer = Screenwriters.query.filter_by(userid = current_user.id).first()
-        return render_template("profilepage.html", writer=writer, user=current_user)
-    if current_user.accounttype == 2:
-        return render_template("producerpage.html")
-''' 
+def pageeditor():
+    form = ProfilePageForm()
+    # if form.validate_on_submit():
+    return render_template("pageeditor.html", form=form, user=current_user)
