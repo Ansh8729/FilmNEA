bfe98f3d9c53be8381e93acc9a022acffb7c4052
Ansh Bindroo <bindrooansh@gmail.com>
2023-12-06 10:35:00 +0400

    made function that reformats dates

--- website/comps.py
+++ website/comps.py
@@ -17,6 +17,39 @@ from . import db
 from dateutil.parser import parse
 from datetime import datetime, timedelta, date
 
+def ISOtoDate(date):
+    months = {
+    "01": "January",
+    "02": "February",
+    "03": "March",
+    "04":"April",
+    "05":"May",
+    "06":"June",
+    "07":"July",
+    "08":"August",
+    "09":"September",
+    "10":"October",
+    "11":"November",
+    "12":"December"}
+    datestr = str(datetime.fromisoformat(date))
+    date2 = list(datestr)[0:10]
+    daynum = ''.join(date2[8]+date2[9])
+    if date2[9] == "1":
+        day = daynum+"st"
+    elif daynum == "02" or daynum == "22":
+        day = daynum+"st"
+    elif daynum == "03" or daynum == "23":
+        day = daynum+"rd"
+    else:
+        day = daynum +"th"
+    if day[0] == "0":
+        day = list(day)
+        day = day[1:6]
+        day = ''.join(day)
+    month = months[''.join(date2[5]+date2[6])]
+    year = ''.join(date2[0:4])
+    return f"{day} {month} {year}"
+
 comps = flask.Blueprint("comps", __name__)
 
 def convert_to_datetime(input_str, parserinfo=None):
--- website/notifs.py
+++ website/notifs.py
@@ -4,6 +4,40 @@ from flask_login import login_required, current_user
 from .models import Users, Screenwriters, Producers, Competitions, Screenplays, LikedScreenplays, Comments, Genres, ScriptHas, CompHas, Notifications, FeaturedScripts
 import uuid as uuid
 from . import db
+import datetime
+
+def ISOtoDate(date):
+    months = {
+    "01": "January",
+    "02": "February",
+    "03": "March",
+    "04":"April",
+    "05":"May",
+    "06":"June",
+    "07":"July",
+    "08":"August",
+    "09":"September",
+    "10":"October",
+    "11":"November",
+    "12":"December"}
+    datestr = str(datetime.fromisoformat(date))
+    date2 = list(datestr)[0:10]
+    daynum = ''.join(date2[8]+date2[9])
+    if date2[9] == "1":
+        day = daynum+"st"
+    elif daynum == "02" or daynum == "22":
+        day = daynum+"st"
+    elif daynum == "03" or daynum == "23":
+        day = daynum+"rd"
+    else:
+        day = daynum +"th"
+    if day[0] == "0":
+        day = list(day)
+        day = day[1:6]
+        day = ''.join(day)
+    month = months[''.join(date2[5]+date2[6])]
+    year = ''.join(date2[0:4])
+    return f"{day} {month} {year}"
 
 notifs = flask.Blueprint("notifs", __name__)
 
